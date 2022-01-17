58be683a3fe3ddaffc73ddf704265bfc3d08dd7e
nanigen <otoo.danieru@gmail.com>
2022-01-17 19:12:54 +1100

    Whittling down...

--- channel-n/packages/channel-n-xyz.scm
+++ channel-n/packages/channel-n-xyz.scm
@@ -30,25 +30,25 @@
        (base32
         "0mphj2nb5660mh4cxv51ivjykzqjrqjrwsz8hpp9sw7c8yrw4qi1")))))))
 
-(define-public python2-progressbar
-  (package
-    (name "python2-progressbar")
-    (version "2.5")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "progressbar" version))
-       (sha256
-        (base32
-         "0qvckfpkdk7a35r9lc201rkwc18grb4ddbv276sj7qm2km9cp0ax"))))
-    (build-system python-build-system)
-    (home-page "https://github.com/niltonvolpato/python-progressbar")
-    (synopsis "Text progress bar library for Python")
-    (description
-     "A text progress bar is typically used to display the progress of a long
-running operation, providing a visual cue that processing is underway.")
-    ;; Either or both of these licenses may be selected.
-    (license (list license:lgpl2.1+ license:bsd-3))))
+;; (define-public python2-progressbar
+;;   (package
+;;     (name "python2-progressbar")
+;;     (version "2.5")
+;;     (source
+;;      (origin
+;;        (method url-fetch)
+;;        (uri (pypi-uri "progressbar" version))
+;;        (sha256
+;;         (base32
+;;          "0qvckfpkdk7a35r9lc201rkwc18grb4ddbv276sj7qm2km9cp0ax"))))
+;;     (build-system python-build-system)
+;;     (home-page "https://github.com/niltonvolpato/python-progressbar")
+;;     (synopsis "Text progress bar library for Python")
+;;     (description
+;;      "A text progress bar is typically used to display the progress of a long
+;; running operation, providing a visual cue that processing is underway.")
+;;     ;; Either or both of these licenses may be selected.
+;;     (license (list license:lgpl2.1+ license:bsd-3))))
 
 ;; python2-progressbar
 python-gyp-patch
